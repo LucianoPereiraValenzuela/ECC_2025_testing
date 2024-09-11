bb82913bc56eba4a2ec08584d84bb8a8ad44d3cd
LucianoPereiraValenzuela <luciano.pereira.valenzuela@gmail.com>
2024-09-11 15:43:39 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="ECC2025",
+    version="0.1.0",
+    description="ECC2025",
+    url="https://github.com/LucianoPereiraValenzuela/ECC_2025_testing",
+    author="Luciano Pereira",
+    author_email="luciano.pereira.valenzuela@gmail.com",
+    license="Apache 2.0",
+    install_requires=["numpy", "scipy", "qiskit", "qiskit_ibm_runtime", "qiskit_aer"],
+)
