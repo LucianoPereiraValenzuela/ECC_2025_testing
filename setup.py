9f128459d7373b6550b45ef1095fdc0f1bba2ef9
LucianoPereiraValenzuela <luciano.pereira.valenzuela@gmail.com>
2024-09-11 18:47:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="ECC2025",
-    version="0.1.1",
+    version="0.1.2",
     description="ECC2025",
     url="https://github.com/LucianoPereiraValenzuela/ECC_2025_testing",
     author="Luciano Pereira",
