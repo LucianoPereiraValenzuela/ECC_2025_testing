f2a893af69435b690ae92472c17cece1e9157fba
LucianoPereiraValenzuela <luciano.pereira.valenzuela@gmail.com>
2024-09-11 15:51:32 +0200

    jklmjk

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name="ECC2025",
-    version="0.1.0",
+    version="0.1.1",
     description="ECC2025",
     url="https://github.com/LucianoPereiraValenzuela/ECC_2025_testing",
     author="Luciano Pereira",
     author_email="luciano.pereira.valenzuela@gmail.com",
     license="Apache 2.0",
-    install_requires=["numpy", "scipy", "qiskit", "qiskit_ibm_runtime", "qiskit_aer"],
+    install_requires=["numpy", "scipy", "qiskit", "qiskit_ibm_runtime", "qiskit_aer", "pylatexenc"],
 )
